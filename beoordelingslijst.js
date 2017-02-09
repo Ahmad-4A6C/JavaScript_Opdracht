7af830cc879a02848ffebae5aa1e1f9cbf8dd38c
Ahmad-4A6C <505263@edu.rocmn.nl>
2017-02-09 11:09:03 +0100

    functie 1 is klaar

---
+++ beoordelingslijst.js
@@ -0,0 +1,25 @@
+function cijfers(nummer) {
+            if (nummer > 0 && nummer <50){
+                return "onvoldoende"
+            }else{
+                if(nummer >=50 && nummer <60) {
+                    return "matig"
+                } else{
+                    if(nummer >=60 && nummer <75) {
+                        return "voldoende"
+                    } else if (nummer >=75 && nummer <101) {
+                       return "goed"
+                    }
+                    
+                }
+            }
+        }
+
+
+function functie1() {
+    var cijfer = document.getElementById("cijfer1").value;
+    
+    var numbers = cijfers(cijfer);
+            
+    document.getElementById("resultaat").innerHTML += "- Het cijfer is " + cijfer + " en is dus "+ numbers+"<br>";  
+}
